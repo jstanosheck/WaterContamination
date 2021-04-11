05f99bf5deb956b4d8e5015abdf8d6b14bfbcea4
Jacob Stanosheck <jstanosheck@tamu.edu>
2021-04-11 17:13:06 -0500

    Added calculations for all oversample methods for log model

--- PeerReviewData.R
+++ PeerReviewData.R
@@ -6,7 +6,9 @@
 
 #Import needed libraries and source from function scripts.
 library(randomForest)
+library(pROC)
 source("Preprocessfunc.R")
+source("modelingfunc.R")
 
 #Save the original data as 'origin_data' from the Florida data set.
 data <- read.csv("Data/FloridaWaterQualityData.csv")
@@ -30,4 +32,81 @@ data <- standardize(data$trainset, data$testset)
 
 #Generate synthetic data for the training data.
 oversampled <- syngen(data$trainset, 'Salmonella', c(1, 2), C = 3)
+
+
+
+
+#Logistic model determination
+################################################################################
+
+#Use logistic_model function to determine the logistic model for each method
+log_outputs <- logistic_model(oversampled, data$testset, 10, c(5, 6, 7, 8, 9), cutoff = 1.432079e-01)
+
+#roc plot for each log_output
+#smote
+roc.info <- pROC::roc(data$testset$Salmonella, log_outputs$smote_prob, plot = T)
+roc.df <- data.frame(
+  tpp = roc.info$sensitivities * 100,
+  fpp = roc.info$specificities * 100,
+  thresholds = roc.info$thresholds)
+roc.df
+
+#make confusion matrix for SMOTE
+table(log_outputs$smote_predict, data$testset$Salmonella)
+
+#adasyn
+roc.info <- pROC::roc(data$testset$Salmonella, log_outputs$adasyn_prob, plot = T)
+roc.df <- data.frame(
+  tpp = roc.info$sensitivities * 100,
+  fpp = roc.info$specificities * 100,
+  thresholds = roc.info$thresholds)
+roc.df
+
+#make confusion matrix for adasyn
+table(log_outputs$adasyn_predict, data$testset$Salmonella)
+
+#SL-SMOTE
+roc.info <- pROC::roc(data$testset$Salmonella, log_outputs$slsmote_prob, plot = T)
+roc.df <- data.frame(
+  tpp = roc.info$sensitivities * 100,
+  fpp = roc.info$specificities * 100,
+  thresholds = roc.info$thresholds)
+roc.df
+
+#make confusion matrix for SL-SMOTE
+table(log_outputs$slsmote_predict, data$testset$Salmonella)
+
+
+#testing with original data
+
+train_control <- caret::trainControl(method = 'cv', number = 10)
+
+#use original data set
+origin_model <- caret::train(data$trainset[, 7:11],
+                            y = data$trainset[, 2],
+                            method = 'glm',
+                            trControl = train_control,
+                            family = binomial())
+
+#use origin model to predict on the test data
+origin_prob <- predict(origin_model$finalModel, newdata = data$testset, type = 'response')
+origin_predict <- ifelse(origin_prob > 0.5, 1, 0)
+
+roc.info <- pROC::roc(data$testset$Salmonella, origin_prob, plot = T)
+roc.df <- data.frame(
+  tpp = roc.info$sensitivities * 100,
+  fpp = roc.info$specificities * 100,
+  thresholds = roc.info$thresholds)
+roc.df
+
+#make confusion matrix for SL-SMOTE
+table(origin_predict, data$testset$Salmonella)
+################################################################################
+
+#Random Forest model determination
+################################################################################
+
+
+################################################################################
+
 
